535d3bed7e674a57bcce6be39f440a1a5e482bf5
rodrigosoberon <rodrigo.soberon.a@gmail.com>
2022-09-25 23:16:57 -0300

    logo y pie de pagina

--- scripts/nav-script.js
+++ scripts/nav-script.js
@@ -7,3 +7,5 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("icono-menu").style.display = "block";
 }
+
+// TODO: Agregar funcionalidad para que se cierre cuando se hace click fuera del menu
