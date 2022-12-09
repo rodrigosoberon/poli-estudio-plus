4e0ab96cdf1c38b7731ebbb4de494200a5f77fd8
rodrigosoberon <rodrigo.soberon.a@gmail.com>
2022-12-09 14:28:17 -0300

    renders con grilla y modal

---
+++ scripts/img-modal.js
@@ -0,0 +1,14 @@
+let modal = document.getElementById("myModal");
+let captionText = document.getElementById("caption");
+
+function expand(img){
+  modal.style.display = "block";
+  modalImg.src = img.src;
+  captionText.innerHTML = img.alt;
+}
+
+let botonCerrar = document.getElementsByClassName("closeModal")[0];
+
+botonCerrar.onclick = function() { 
+  modal.style.display = "none";
+}
